d121436892792df15455e5c8df2243ed9afeaa86
nira07 <63848451+nira07@users.noreply.github.com>
2021-07-16 11:36:22 +0530

    Add files via upload

---
+++ project_calc.cpp
@@ -0,0 +1,43 @@
+# include <iostream>
+using namespace std;
+
+int main()
+{
+    char op;
+    float num1, num2;
+
+    cout << "Enter operator either + or - or * or /: ";
+    cin >> op;
+
+    cout << "Enter first operand: ";
+    cin >> num1 ;
+    cout << "Enter second operand: ";
+    cin>> num2 ; 
+
+    switch(op)
+    {
+        case '+':
+            cout << num1+num2;
+        
+            break;
+
+        case '-':
+            cout << num1-num2;
+            break;
+
+        case '*':
+            cout << num1*num2;
+            break;
+
+        case '/':
+            cout << num1/num2;
+            break;
+
+         default:
+            // If the operator is other than +, -, * or /, error message is shown
+            cout << "Error! operator is not correct";
+            break;
+    }
+
+    return 0;
+}
